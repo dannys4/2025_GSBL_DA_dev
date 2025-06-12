efc1facaed07f3e7172ddbc9d1df8f511cdeea86
Daniel Sharp <dannys4@mit.edu>
2025-06-12 10:21:20 -0400

    Add more flexible CRPS

--- src/tools/crps.jl
+++ src/tools/crps.jl
@@ -43,3 +43,46 @@ function CRPS(X::AbstractVector, x::Float64)
     return term1 - 0.5*term2
 end
 
+
+function CRPS(X::AbstractMatrix, x::AbstractVector, which_norm = :norm2)
+    d, N = size(X)
+    abs_fcn, rt_fcn = nothing, nothing
+    if which_norm == :norm2
+        abs_fcn, rt_fcn = abs2, sqrt
+    elseif which_norm == :norm1
+        abs_fcn, rt_fcn = abs, identity
+    else
+        throw(ArgumentError("Which norm: only :norm2 and :norm1 are supported"))
+    end
+    rmse = 0.
+    std_diff = 0.
+    for ens_idx in 1:N
+        sq_diff = zero(eltype(x))
+        for dim_idx in 1:d
+            sq_diff += abs_fcn(X[dim_idx,ens_idx] - x[dim_idx])
+        end
+        rmse += rt_fcn(sq_diff)
+        for ens_idx_prime in ens_idx+1:N
+            var_diff = zero(eltype(x))
+            for dim_idx in 1:d
+                var_diff += 2abs_fcn(X[dim_idx,ens_idx] - X[dim_idx,ens_idx_prime])
+            end
+            std_diff += rt_fcn(var_diff)
+        end
+    end
+    rmse /= N
+    std_diff /= N*(N-1)
+    crps = rmse - std_diff/2
+    (;crps, rmse, std_diff)
+end
+
+# Return CRPS of a trajectory at each time
+function CRPS(X::AbstractVector{<:AbstractMatrix}, truth::AbstractMatrix)
+    d,T = size(truth)
+    @assert length(X) == T+1
+    map(enumerate(X[2:end])) do (i, Xi)
+        mean(axes(truth,1)) do j
+            CRPS(@view(Xi[j,:]),truth[j,i])
+        end
+    end
+end
