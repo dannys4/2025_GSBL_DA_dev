92cde649f00826b32b63b72deaa001a52ef1e093
Daniel Sharp <dannys4@mit.edu>
2025-11-19 16:27:49 -0500

    Work on separate theta

--- src/update_x/hierarchical_separate.jl
+++ src/update_x/hierarchical_separate.jl
@@ -1,93 +1,99 @@
-# In this version, there is a jump coefficient θ per ensemble member
+# In this version, there is a jump coefficient θ shared across the ensemble members
 
 function update_x!(
     enkf::HierarchicalSeqFilter,
     X_forecast,
-    θ::Matrix{Float64},
     perturbed_obs::Matrix{Float64},
+    ĈX_op,
+    θ::Matrix{Float64},
     t,
     X_analysis,
     verbose::Bool
 )
-    @assert !enkf.isθshared
+    verbose && @info "x being updated"
+    @assert enkf.isθshared
 
-    Ny = size(ystar, 1)
-    Nx = size(X_forecast, 1) - Ny
+    # Update weight vector θ
+    if !(enkf.sys.Cθ isa LinearMaps.LinearMaps.WrappedMap{Float64})
+        ArgumentError("Wrong type for Cθ")
+    end
+
+    Ny = size(perturbed_obs, 1)
+    Nx = size(X_forecast, 1)
     Ne = size(X_forecast, 2)
-    Ns = enkf.sys.Ns
+    Ne = size(X_forecast, 2)
+    Nz = enkf.sys.Nz
 
-    @assert size(θ, 1) == Ns
-    @assert size(θ, 2) == Ne
-    @assert size(perturbed_obs, 1) == Ny
+    @assert size(θ) == (Nz, Ne)
     @assert size(perturbed_obs, 2) == Ne
 
-    # Generate observational noise samples
-    # E = zeros(Ny, Ne)
-    # if enkf.ϵy isa AdditiveInflation
-    #     E .= enkf.ϵy.σ * randn(Ny, Ne) .+ enkf.ϵy.m
-    # end
-
-    si = zeros(enkf.sys.Ns)
-
-    ĈX = getĈX(enkf, X_forecast, Nx, Ny)
-
-    ĈX_op = FunctionMap{Float64,true}(
-        (y, x) -> mul!(y, ĈX, x),
-        Nx;
-        issymmetric=true,
-        isposdef=false,
-    )
+    verbose && @info "noise sampled"
 
     # Update covariance matrix
     enkf.sys.CX = ĈX_op
+    verbose && @info "CX copied"
 
-    enkf.sys.Cθ isa LinearMaps.LinearMaps.WrappedMap{Float64} || throw(ArgumentError("Wrong type of theta!"))
+    # copy!(enkf.θ, θ)
+    fill!(enkf.θ, 0.)
+    # verbose && @info "theta copied"
+    # copy!(enkf.sys.Cθ.lmap.diag, θ)
+    fill!(enkf.sys.Cθ.lmap.diag, 0.)
+    # verbose && @info "Getting sys op"
 
-    sys_op = LinearMaps.FunctionMap{Float64,true}(
-        (y, x) -> mul!(y, enkf.sys, x),
-        Ny + Ns;
-        issymmetric=true,
-        isposdef=true,
-    )
+    if enkf.isiterative
+        sys_op = enkf.sys
+        precond = Diagonal(sys_op)
+        cg_out = zeros(length(ys_i))
+    else
+        sys_mat = Matrix(enkf.sys)
+        sys_fac = similar(sys_mat)
+        diag_theta_fac = diagind(sys_fac)[end-Nz+1:end]
+    end
+    verbose && @info "Got sys op"
 
-    if X_forecast !== X_analysis
+    # Compute Kalman-update in a matrix-free way
+    ys_i = ObsConstraintVector(Ny, Nz)
+    tmp = zeros(length(ys_i))
+
+    δi = zeros(Nx)
+
+    if X_analysis !== X_forecast
         copy!(X_analysis, X_forecast)
     end
 
-    if !enkf.isiterative
-        sys_mat = factorize(Symmetric(Matrix(sys_op)))
-    end
-
-
     for i = 1:Ne
-        # Compute Kalman-update in a matrix-free way
-
-        ys_i = ObsConstraintVector(Ny, Ns)
-        tmp = ObsConstraintVector(Ny, Ns)
-
-        δi = zeros(Nx)
-
-        xi = @view X_analysis[:, i]
-        obs_i = @view perturbed_obs[:, i]
+        verbose && @info "Ensemble member $i"
+        fill!(ys_i, zero(eltype(ys_i)))
+        fill!(δi, zero(eltype(δi)))
+        xi_forecast = @view X_forecast[:, i]
+        xi_analysis = @view X_analysis[:, i]
         yi = observation(ys_i)
         si = constraint(ys_i)
 
+        obs_i = @view perturbed_obs[:, i]
+        # yi .= obs_i - enkf.sys.H * xi_forecast
+        # si .= -enkf.sys.S * xi_forecast
         copy!(yi, obs_i)
-        mul!(yi, enkf.sys.H, xi, -1, true)
-        # @assert isapprox(ys_i.x[1], enkf.sys.H * xi, atol=1e-8)
-
-        mul!(si, enkf.sys.S, xi)
-
+        mul!(yi, enkf.sys.H, xi_forecast, -1, true)
+        mul!(si, enkf.sys.S, xi_forecast, -1, false)
+        copy!(tmp, ys_i)
+        verbose && @info "Start solve"
         if enkf.isiterative
+            copy!(enkf.sys.Cθ.lmap.diag, @view(θ[:, i]))
             # Invert sys_op
-            cg!(ys_i, sys_op, copy(ys_i); log=false, reltol=enkf.cg_tol)
+            cg!(cg_out, sys_op, tmp; log=false, verbose=false, reltol=enkf.cg_tol, Pl=precond)
+            copy!(ys_i, cg_out)
         else
-            ldiv!(sys_mat, ys_i)
+            copy!(sys_fac, sys_mat)
+            sys_fac[diag_theta_fac] .+= @view θ[:, i]
+            fac = bunchkaufman!(sys_fac)
+            ldiv!(fac, tmp)
+            copy!(ys_i, tmp)
         end
-
-        δi .= enkf.sys.H' * observation(ys_i)
-        δi .+= enkf.sys.S' * constraint(ys_i)
-
-        xi .+= (ĈX * δi)
+        # δi .= enkf.sys.H' * observation(ys_i) + enkf.sys.S' * constraint(ys_i)
+        # xi_analysis .= xi_analysis + ĈX_op * δi
+        mul!(δi, enkf.sys.H', observation(ys_i))
+        mul!(δi, enkf.sys.S', constraint(ys_i), true, true)
+        mul!(xi_analysis, ĈX_op, δi, true, true)
     end
 end
